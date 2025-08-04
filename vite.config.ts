8ce1dde2ce7572dfebfb3b53e2cd7a51927bd1b7
Mjjafari66 <132228351+Mjjafari66@users.noreply.github.com>
2025-08-04 12:39:22 +0330

    mj

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,5 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
  //
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/gilded-grace/',
 }));
